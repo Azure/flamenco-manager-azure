a05aef3ee6b048f8bc02d2afe01744a5c37a85be
Sybren A. Stüvel <sybren@stuvel.eu>
2019-04-23 12:59:05 +0200

    Generate random worker secret

--- flamenco/template.go
+++ flamenco/template.go
@@ -2,6 +2,8 @@ package flamenco
 
 import (
 	"bytes"
+	"crypto/rand"
+	"encoding/base64"
 	"path"
 	"runtime"
 	"strings"
@@ -19,15 +21,26 @@ type TemplateContext struct {
 	AcmeDomainName           string
 	PrivateIP                string
 	WorkerRegistrationSecret string
+	FSTabForStorage          string
 }
 
 // NewTemplateContext constructs a new context for rendering templated config files.
-func NewTemplateContext(config azconfig.AZConfig, netStack aznetwork.NetworkStack) TemplateContext {
+func NewTemplateContext(
+	config azconfig.AZConfig,
+	netStack aznetwork.NetworkStack,
+	fstab string,
+) TemplateContext {
+	randomBytes := make([]byte, 64)
+	if _, err := rand.Read(randomBytes); err != nil {
+		logrus.WithError(err).Fatal("error reading random bytes")
+	}
+
 	ctx := TemplateContext{
-		strings.Title(config.VMName),
-		netStack.FQDN(),
-		netStack.PrivateIP,
-		"", // TODO: generate secret and store in Worker config as well.
+		Name:                     strings.Title(config.VMName),
+		AcmeDomainName:           netStack.FQDN(),
+		PrivateIP:                netStack.PrivateIP,
+		WorkerRegistrationSecret: base64.URLEncoding.EncodeToString(randomBytes),
+		FSTabForStorage:          fstab,
 	}
 	return ctx
 }
