4f5e44a8eaee7d77b6fda0ef22c6c593decddda9
Sybren A. Stüvel <sybren@stuvel.eu>
2019-07-23 15:01:54 +0200

    Some improvements to the release script
    - Refactored some of the HTTP request handling.
    - Improved the JSON response parsing.
    - Added query string encoding of the to-be-uploaded filename.

--- release/release.go
+++ release/release.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"path/filepath"
@@ -14,12 +15,12 @@ import (
 	"time"
 
 	"github.com/sirupsen/logrus"
-	"gopkg.in/mgo.v2/bson"
 )
 
 const (
-	appName   = "Flamenco Manager Azure"
-	githubURL = "https://api.github.com/repos/Azure/flamenco-manager-azure/releases"
+	appName = "Flamenco Manager Azure"
+	// githubURL = "https://api.github.com/repos/Azure/flamenco-manager-azure/releases"
+	githubURL = "https://api.github.com/repos/sybrenstuvel/privateproject/releases"
 )
 
 type githubRelease struct {
@@ -28,29 +29,39 @@ type githubRelease struct {
 	Body    string `json:"body"`
 }
 
+// githubReleaseCreateResponse is received from GitHub,
+// see https://developer.github.com/v3/repos/releases/#create-a-release
+type githubReleaseCreateResponse struct {
+	UploadURL string `json:"upload_url"`
+	Name      string `json:"name"`
+}
+
 var cliArgs struct {
 	version  string
 	fileglob string
 }
+
+var client http.Client
 
 func main() {
 	parseCliArgs()
 
 	bytes := makeRequestPayload()
 	os.Stdout.Write(bytes)
+	os.Stdout.Write([]byte("\n"))
+
+	client = http.Client{
+		Timeout: 1 * time.Minute,
+	}
 
 	token := loadPersonalAccessToken()
-	token = "token " + token
-
 	resp := doGithubCreateRelease(bytes, token, githubURL)
-
-	if resp["upload_url"] == nil {
-		logrus.Fatal("upload_url not found")
+	if resp.UploadURL == "" {
+		logrus.Fatal("upload_url not found in response JSON")
 	}
 
 	assets := getUploadableAssets()
-
-	doGitHubUploadAssets(assets, token, resp["upload_url"].(string))
+	doGitHubUploadAssets(assets, token, resp.UploadURL)
 
 	os.Stdout.Write([]byte("\n"))
 }
@@ -82,6 +93,17 @@ func makeRequestPayload() []byte {
 	return bytes
 }
 
+func makeRequest(url, authToken string, body []byte) *http.Request {
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		logger := logrus.WithField("url", url)
+		logger.WithError(err).Fatal("unable to create HTTP request")
+	}
+	req.Header.Set("Authorization", "token "+authToken)
+
+	return req
+}
+
 // loadPersonalAccessToken loads the Github access token from .githubAccessToken.
 func loadPersonalAccessToken() string {
 	fname, err := filepath.Abs(".githubAccessToken")
@@ -100,19 +122,11 @@ func loadPersonalAccessToken() string {
 
 // doGithubCreateRelease sends a payload to Github, authenticated with the user's token.
 // The response is assumed to be JSON, and shown nicely formatted on stdout.
-func doGithubCreateRelease(payload []byte, authToken string, url string) bson.M {
-	client := http.Client{
-		Timeout: 1 * time.Minute,
-	}
-
+func doGithubCreateRelease(payload []byte, authToken string, url string) githubReleaseCreateResponse {
 	logger := logrus.WithField("url", url)
-	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
-	// req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		logger.WithError(err).Fatal("unable to create HTTP request")
-	}
+
+	req := makeRequest(url, authToken, payload)
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", authToken)
 
 	resp, err := client.Do(req)
 	if err != nil {
@@ -130,9 +144,8 @@ func doGithubCreateRelease(payload []byte, authToken string, url string) bson.M 
 		logger.WithField("body", string(body)).Fatal("non-JSON response from GitHub")
 	}
 
-	response := bson.M{}
+	response := githubReleaseCreateResponse{}
 	unmarshaller := json.NewDecoder(resp.Body)
-
 	if err := unmarshaller.Decode(&response); err != nil {
 		logger.WithError(err).Fatal("error reading/parsing HTTP response")
 	}
@@ -141,8 +154,9 @@ func doGithubCreateRelease(payload []byte, authToken string, url string) bson.M 
 		logger.WithError(err).Error("unable to nicely format response")
 		fmt.Printf("%#v\n", response)
 	} else {
-		logger.Info("Response from GitHub:")
+		logger.Info("Response from GitHub (well, the fields we're using):")
 		os.Stdout.Write(niceResponse)
+		os.Stdout.Write([]byte("\n"))
 	}
 	return response
 }
@@ -170,40 +184,32 @@ func doGitHubUploadAssets(paths []string, authToken string, url string) {
 }
 
 // doGitHubUploadAsset uploads an asset to the GitHub release
-func doGitHubUploadAsset(fpath string, authToken string, url string) {
+func doGitHubUploadAsset(fpath string, authToken string, assetUploadURL string) {
 	file, err := ioutil.ReadFile(fpath)
 	if err != nil {
 		panic(err)
 	}
 
-	// Get the file name and use it to build the upload URL
+	// Get the file name and use it to build the upload URL. GitHub gives us a URL with
+	// "{?name,label}" in there and expects us to replace that.
 	fname := path.Base(fpath)
-	nameSuffix := "?name=" + fname
-	url = strings.Replace(url, "{?name,label}", nameSuffix, -1)
+	nameSuffix := "?name=" + url.QueryEscape(fname)
+	uploadURL := strings.Replace(assetUploadURL, "{?name,label}", nameSuffix, -1)
 
-	logger := logrus.WithField("url", url)
-	logger.Info("Uploading file")
-	req, err := http.NewRequest("POST", url, bytes.NewReader(file))
-	if err != nil {
-		logger.WithError(err).Fatal("unable to create HTTP request")
-	}
+	req := makeRequest(uploadURL, authToken, file)
 	req.Header.Set("Content-Type", "application/zip")
-	req.Header.Set("Authorization", authToken)
 
-	// Construct a Client and perform the upload request
-	client := http.Client{
-		Timeout: 1 * time.Minute,
-	}
+	logger := logrus.WithField("uploadURL", uploadURL)
+	logger.Info("Uploading file")
 	resp, err := client.Do(req)
 	if err != nil {
 		logger.WithError(err).Fatal("error performing HTTP request")
 	}
 
-	statusCode := resp.StatusCode
-	if statusCode == 201 {
-		logger.WithField("statusCode", resp.StatusCode).Info("Asset uploaded successfully")
+	logger = logger.WithField("statusCode", resp.StatusCode)
+	if resp.StatusCode == 201 {
+		logger.Info("Asset uploaded successfully")
 	} else {
-		logger.WithField("statusCode", resp.StatusCode).Error("Unable to upload asset")
+		logger.Error("Unable to upload asset")
 	}
-
 }
